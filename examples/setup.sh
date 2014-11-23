cf017e22dfe3a6d93b07f4a99b0e6094f0b15618
Makis Tracend <makis.tracend@gmail.com>
2014-11-22 19:42:08 -0800

    scaffolding

---
+++ examples/setup.sh
@@ -0,0 +1,15 @@
+
+# Installing the Chiiv CLI
+
+# 1. Install the main executable
+npm install -g chiiv-cli
+
+#2. Setup Chiiv (only needed once)
+chiiv setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+chiiv -h
